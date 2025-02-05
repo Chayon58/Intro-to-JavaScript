d9d6cace5d848633d447744ea739bc47d05a1dc5
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-05 22:17:10 +0600

    added condition

--- variable.js
+++ variable.js
@@ -1,21 +1,34 @@
+
 /**
- * VARIABLE ==> vary + able
- * 5 things to declare a variable
+ * VARIABLE ==> container
 */
 
-var w = 38;
-var p = 38;
-var year = 1995;
-var money = 2005;
-money = 500;
-var age;
-sum = w+p;
-console.log(sum)
-console.log(money);
-console.log(p);
+// number
+var sunglassPrice = 80;
 
+// string
+var subject = "EEE";
 
-var age = 17;
-age = 35;
+// boolean
+var passed = true;
 
-console.log(age);
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+*/
+
+var lastExamResult = 89;
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN 
+ * 4. null
+ * 5. Number 
+*/
