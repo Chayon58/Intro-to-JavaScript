90530833ad0826a4f59adcbfc6017ec163d15749
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-05 11:46:01 +0600

    added variable.js and math.js

---
+++ variable.js
@@ -0,0 +1,15 @@
+/**
+ * VARIABLE ==> vary + able
+ * 5 things to declare a variable
+*/
+
+var w = 38;
+var p = 38;
+var year = 1995;
+var money = 2005;
+money = 500;
+var age;
+sum = w+p;
+console.log(sum)
+console.log(money);
+console.log(p);
