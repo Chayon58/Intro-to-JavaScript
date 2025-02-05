cf01b845adcafa44edba82469f7b9dd0adcfed81
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-05 14:46:58 +0600

    added numbers

---
+++ numbr.js
@@ -0,0 +1,14 @@
+var orangsPrice = 20;
+var chocolate = 0.5;
+var applePrice =parseInt  ('30');
+var applePrice = parseFloat ('30.5');
+
+// console.log(typeof applePrice);
+// console.log(typeof orangsPrice);
+// console.log(orangsPrice + applePrice);
+
+
+var first = 0.1;
+var second = 0.2;
+var total = first+second;
+console.log(total.toFixed(5));
