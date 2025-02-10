1c942692758c97d0bd159a52bced8705dcfc2234
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-10 23:26:58 +0600

    added string

---
+++ string.js
@@ -0,0 +1,6 @@
+const country = 'United States';
+const division = "California";
+const city = `San Francisco`;
+// const thana = new String('Dhaka');
+
+console.log(country, division, city); // United States California San Francisco
