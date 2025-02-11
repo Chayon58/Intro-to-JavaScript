72a37aca8cafc9d86607ccf91cc3706133f26242
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-11 23:24:15 +0600

    added property

---
+++ property.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'sodor uddin', 
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+    'fav places': ['bandorban', 'saintmartin', 'kuakata']
+}
+
+ console.log(person)
+ console.log(person.profession)
