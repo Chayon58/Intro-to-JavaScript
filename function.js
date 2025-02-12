029ffc3f62e81169ab9f9860e22d478d8ab98e76
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-12 21:27:01 +0600

    added function

---
+++ function.js
@@ -0,0 +1,23 @@
+const number = 1;
+
+function fanOffkor(){
+    console.log('bosha theke uthe dara');
+    console.log('walk towards the kitchen');
+    console.log('click the switch to the kitchen');
+}
+
+//call the function
+fanOffkor();
+
+// Function Name (){
+//     
+// }
+
+
+function brushTeeth(){
+    console.log('take the brush');
+    console.log('apply toothpaste');
+    console.log('brush the teeth');
+}
+
+brushTeeth();
