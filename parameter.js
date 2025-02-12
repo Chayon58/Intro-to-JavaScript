6f2dae08867f17eca23971ef95c15627c8ec9ded
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-13 02:15:00 +0600

    added parameter

--- parameter.js
+++ parameter.js
@@ -5,5 +5,29 @@ function vaatKhao(){
     console.log("bhat er upor vaat dao!");
 }
 
-vaatKhao();
+// vaatKhao();
 
+
+// 3 --> 3*3 = 9
+// 4 --> 4*4 = 16
+// 5 --> 5*5 = 25
+// 11 --> 11*11 = 121
+
+function square(number) {
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('square of the given number is: ', borgo);
+}
+square(4);
+console.log('-----------------------------------------------------------------');   
+square(12);
+square(5);
+square(405);
+
+function add(num1,num2){
+    const sum = num1 + num2;
+    console.log('sum of the two numbers is: ', sum);
+}
+
+add(5, 71);
+add(65, 31);
