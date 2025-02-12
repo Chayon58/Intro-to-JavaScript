eaf8e40ec1741d30d5b88b9cb83b65ac12dde6a2
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-13 02:06:56 +0600

    added parameter

---
+++ parameter.js
@@ -0,0 +1,9 @@
+function vaatKhao(){
+    console.log("wash your hands first!");
+    console.log("bosho");
+    console.log("plate er kabar dabar nao!");
+    console.log("bhat er upor vaat dao!");
+}
+
+vaatKhao();
+
