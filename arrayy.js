429828d408b15d7c937fb8b3125016f060ee3a50
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-02-20 22:22:02 +0600

    added arrays

---
+++ arrayy.js
@@ -0,0 +1,20 @@
+// const products = [
+//     { name: 'laptop', price: 1000 },
+//     { name: 'desktop', price: 1500 },
+//     { name: 'phone', price: 500 },
+//     { name: 'tablet', price: 800 }
+// ]
+
+// products[0];
+
+// const products = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+const products = {
+    name: 'laptop',
+    price: 1000,
+    name: 'desktop',
+    price: 1500,
+    name: 'phone',
+    price: 500,
+    name: 'tablet',
+    price: 800
+}
---
+++ match.js
@@ -0,0 +1,33 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+for (const number of numbers) {
+  console.log(number);
+}
+
+const products = [
+    { name: 'laptop', price: 1000 },      
+    { name: 'desktop', price: 1500 },
+    { name: 'phone', price: 500 },
+    { name: 'tablet', price: 300 },
+    { name: 'watch', price: 200 }
+];
+
+// for (const product of products) {
+//   console.log(product);
+// }
+
+function matchproducts(products, search) {
+    const match =[]
+  for (const product of products) {
+    if (product.name.includes(search)) {
+      match.push(product);
+    }
+    return match;
+  }
+
+const result = matchproducts(products, 'phone');
+console.log(result);
